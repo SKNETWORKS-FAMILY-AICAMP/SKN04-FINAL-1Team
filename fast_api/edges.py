4c4d88d02137d385293838e8f06a06d636502465
hwarange <asdf78964@naver.com>
2025-01-31 17:07:00 +0900

    fix: 노드 개수가 늘어나서 코드 수정

--- fast_api/edges.py
+++ fast_api/edges.py
@@ -1,8 +1,8 @@
 from nodes import (
     RealEstateState,
-    filter_node, re_questions, extract_keywords_based_on_db,
-    generate_query, clean_sql_response, run_query, generate_response,
-    fiter_router, summarize_conversation, should_summarize
+    filter_node, re_questions, search_similar_questions, extract_keywords_based_on_db,
+    generate_query, clean_sql_response, run_query, no_result_answer, generate_response,
+    query_router, fiter_router, summarize_conversation, should_summarize, clean_result_query
 )
 from utils import memory
 from langgraph.graph import StateGraph, START, END
@@ -12,16 +12,19 @@ workflow = StateGraph(RealEstateState)
 workflow.add_node("Filter Question", filter_node)
 workflow.add_node("Summary", summarize_conversation)
 workflow.add_node('Re_Questions', re_questions)
+workflow.add_node('Search_Similar_Questions', search_similar_questions)
 workflow.add_node('Extract_keywords_based_on_db', extract_keywords_based_on_db)
 workflow.add_node('Generate_Query', generate_query)
 workflow.add_node('Clean_Sql_Response', clean_sql_response)
 workflow.add_node('Run_Query', run_query)
+workflow.add_node('No_Result_Answer', no_result_answer)
+workflow.add_node('Clean_results', clean_result_query)
 workflow.add_node('Generate_Response', generate_response)
 
 workflow.add_conditional_edges(
     "Filter Question",
     fiter_router,
-    { 'Pass': "Extract_keywords_based_on_db", 'Fail': 'Re_Questions'}
+    { 'Pass': "Search_Similar_Questions", 'Fail': 'Re_Questions'}
 )
 
 workflow.add_conditional_edges(
@@ -30,12 +33,20 @@ workflow.add_conditional_edges(
     {"summarize_conversation": "Summary", "Filter Question":"Filter Question"}
 )
 
+workflow.add_conditional_edges(
+    "Run_Query",
+    query_router,
+    {"결과없음": "No_Result_Answer", "결과있음":"Clean_results"}
+)
+
 workflow.add_edge("Summary", "Filter Question")
-workflow.add_edge("Re_Questions", "Filter Question")
+workflow.add_edge("Re_Questions", END)
+workflow.add_edge("Search_Similar_Questions", "Extract_keywords_based_on_db")
 workflow.add_edge("Extract_keywords_based_on_db", "Generate_Query")
 workflow.add_edge("Generate_Query", "Clean_Sql_Response")
 workflow.add_edge("Clean_Sql_Response", "Run_Query")
-workflow.add_edge("Run_Query", "Generate_Response")
+workflow.add_edge("No_Result_Answer", END)
+workflow.add_edge("Clean_results", "Generate_Response")
 workflow.add_edge("Generate_Response", END)
 
 llm_app = workflow.compile(checkpointer=memory)
